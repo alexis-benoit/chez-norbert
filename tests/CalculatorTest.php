4fe22b117332372b58ae0910cf4e3994dc38f838
botflux <victor.mendele68@gmail.com>
2019-09-15 20:52:45 +0200

    :bug: test: add a sample test case

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd () {
+        $this->assertEquals(42, 42);
+    }
+}
