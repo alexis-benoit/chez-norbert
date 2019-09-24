351e8e40cbbd8d9caf4a35752cf4ac86ee5c98b4
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-09-24 18:59:40 +0200

    :star2: feat: add a validation on email field

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -21,6 +22,16 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email."
+     * )
+     * @Assert\Length(
+     *     min = 5,
+     *     max = 180,
+     *     minMessage = "Your email must be at least {{ limit }} characters long",
+     *     maxMessage = "Your email cannot be longer than {{ limit }} characters"
+     * )
      */
     private $email;
 
