ead8604b65d1232a393dacd0461f3a4e3cb29862
botflux <victor.mendele68@gmail.com>
2019-10-01 22:00:55 +0200

    :pencil: docs: add comments in menu component initialize menu

--- assets/site/components/header/initialize-menu.js
+++ assets/site/components/header/initialize-menu.js
@@ -1,13 +1,13 @@
 import handleMenuState from './handle-menu-state'
 
 /**
+ * Initialize menu component
  *
- * @param {Function} subscribe
- * @param {Function} getAttribute
- * @param {Function} setAttribute
+ * @param {Function} subscribe Function that wraps dom event listener
+ * @param {Function} getAttribute Function that wraps dom get attribute
+ * @param {Function} setAttribute Function that wraps dom set attribute
  */
 const initializeMenu = (subscribe, getAttribute, setAttribute) => {
-
     subscribe(() => {
         const currentState = ('true' === getAttribute())
         setAttribute (handleMenuState(currentState))
