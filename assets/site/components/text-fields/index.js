497ae1b7b5e3daf9691fc59e89da8cefc1222998
botflux <victor.mendele68@gmail.com>
2019-10-01 21:09:21 +0200

    :herb: refactor: refactor text field scripts to abstract dom uses

--- assets/site/components/text-fields/index.js
+++ assets/site/components/text-fields/index.js
@@ -1,9 +1,20 @@
 import initializeTextField from './initialize-text-field'
 
+/**
+ * Initialize text field module
+ *
+ * @param document
+ */
 const initializeTextFieldAdapter = ({ document }) => {
     const textFields = document.querySelectorAll('.form-control[type="text"]')
 
-    initializeTextField(textFields)
+    textFields.forEach(textField => {
+        const subscribe = callback => textField.addEventListener('blur', callback)
+        const setAttribute = newAttributeValue => textField.setAttribute('data-empty', newAttributeValue)
+        const getTextLength = () => textField.value.length
+
+        initializeTextField(subscribe, setAttribute, getTextLength)
+    })
 }
 
 export default initializeTextFieldAdapter
