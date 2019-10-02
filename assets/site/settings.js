e57cc6438611d9fcc1b4f12cc27ced3b37a6fbf6
botflux <victor.mendele68@gmail.com>
2019-10-02 21:03:31 +0200

    :star2: feat: create a settings file containing leaflet configuration

---
+++ assets/site/settings.js
@@ -0,0 +1,7 @@
+export default {
+    map: {
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution: '\'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors\'',
+        maxZoom: 18
+    }
+}
