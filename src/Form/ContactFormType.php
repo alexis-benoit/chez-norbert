b42de19689e8fdc5bdebffc9d666fe78475d0096
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-07 21:48:34 +0200

    :star2: feat: create base contact form

---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('LastName', TextType::class)
+            ->add('FirstName', TextType::class)
+            ->add('Email', TextType::class)
+            ->add('Tel', TextType::class)
+            ->add('Message', TextType::class)
+            ->add('save', SubmitType::class)
+    ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+}
