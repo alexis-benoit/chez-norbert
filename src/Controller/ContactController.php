b5ac14b21f94c62ab6294375cc00f4810da18854
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-07 21:25:32 +0200

    :star2: feat: create contact entity

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="app_contact")
+     *
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     *
+     * @return Response
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, UserRepository $repository, EntityManagerInterface $manager): Response
+    {
+        if ($repository->getCount() > 0){
+            //TODO Rediriger ves page login quand elle sera faite
+            return $this->redirectToRoute('home.index');
+        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $plainPassword = $form->get('plainPassword')->getData();
+            $encodedPassword = $passwordEncoder->encodePassword($user, $plainPassword);
+
+            $user
+                ->setPassword($encodedPassword)
+                ->setRoles([ 'ROLE_ADMIN' ]);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            // TODO: When the user is created, redirect him to login page.
+            return $this->redirectToRoute('home.index');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
