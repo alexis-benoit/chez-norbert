54325fd3df28cb6564ecdfd89f2234886894907d
Victor Mendele <victor.mendele68@gmail.com>
2019-10-08 20:33:14 +0200

    :herb: refactor: use right form field types in contact form

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -18,8 +20,8 @@ class ContactFormType extends AbstractType
         $builder
             ->add('LastName', TextType::class)
             ->add('FirstName', TextType::class)
-            ->add('Email', TextType::class)
-            ->add('Tel', TextType::class)
+            ->add('Email', EmailType::class)
+            ->add('Tel', TelType::class)
             ->add('Message', TextareaType::class)
             ->add('Send', SubmitType::class)
     ;
