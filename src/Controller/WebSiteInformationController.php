09d1a242940cbe4a796f59e82746e9e76ff32922
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-08 21:33:48 +0200

    :star2: feat: add route to get informations

---
+++ src/Controller/WebSiteInformationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\WebSiteInformationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class WebSiteInformationController extends AbstractController
+{
+    /**
+     * @Route("/information", name="information")
+     * @param Request $request
+     * @param WebSiteInformationRepository $repository
+     * @param EntityManagerInterface $manager
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function getInformation(Request $request, WebSiteInformationRepository $repository, EntityManagerInterface $manager, SerializerInterface $serializer)
+    {
+        $Object = $repository->findOne();
+
+        $json = $serializer->serialize($Object, 'json');
+
+        $response = new Response();
+        $response->setContent($json);
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
