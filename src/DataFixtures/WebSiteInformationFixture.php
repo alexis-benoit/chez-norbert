5d1c8ec62996d55d484728b2cfe2b6fa0c370482
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-08 21:32:40 +0200

    :star2: feat: add fixture to pupulate informations

---
+++ src/DataFixtures/WebSiteInformationFixture.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\WebSiteInformation;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class WebSiteInformationFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $site = new WebSiteInformation();
+        $site->setSiteName('Chez Norbert')
+            ->setFirstName('Prenom')
+            ->setLastName('Nom')
+            ->setEmail('norbert@test.fr')
+            ->setAddress('Bergheim')
+            ->setPhoneNumber('06 06 06 06 06');
+
+        $manager->persist($site);
+        $manager->flush();
+    }
+}
