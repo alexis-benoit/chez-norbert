9d566f4b8855e4260024be228e17618d55708f14
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-14 21:14:49 +0200

    :star2: feat: create repository for gite et chambre d'hote

---
+++ src/Repository/GiteRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Gite;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Gite|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Gite|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Gite[]    findAll()
+ * @method Gite[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class GiteRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Gite::class);
+    }
+
+    public function findOneById($value): ?Gite
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.id = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
+
+    public function findOneByName($value): ?Gite
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.name = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
+    // /**
+    //  * @return Gite[] Returns an array of Gite objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('g.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Gite
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
