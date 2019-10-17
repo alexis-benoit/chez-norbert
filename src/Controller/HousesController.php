4420107fc28c1e1d38cbc53fc1da8b5c090cc2b7
botflux <victor.mendele68@gmail.com>
2019-10-17 23:28:00 +0200

    :star2: feat: add dynamic route for houses

--- src/Controller/HousesController.php
+++ src/Controller/HousesController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\House;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HousesController extends AbstractController
 {
     /**
-     * @Route("/houses", name="houses.get")
+     * @Route("/houses/{id}", name="houses.get")
+     *
+     * @param House $house
+     * @return Response
      */
-    public function index()
+    public function index(House $house)
     {
-        return $this->render('houses/index.html.twig');
+        return $this->render('houses/index.html.twig', [
+            'house' => $house
+        ]);
     }
 }
