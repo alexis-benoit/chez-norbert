5cd406ffc9cd40a24e7b93337692273affc32415
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-17 22:08:29 +0200

    star2: feat: add index and delete route and views for houses

--- src/Controller/Admin/AdminHouseController.php
+++ src/Controller/Admin/AdminHouseController.php
@@ -7,6 +7,7 @@ use App\Form\HouseType;
 use App\Repository\HouseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,10 +20,33 @@ class AdminHouseController extends AbstractController
 {
     /**
      * @Route({
+     * "fr": "admin/house",
+     * "en": "admin/house"
+     * }, name="house.index")
+     *
+     * @param HouseRepository $houseRepository
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function index(HouseRepository $houseRepository, Request $request)
+    {
+        $houses =  $houseRepository->FindAll();
+
+        return $this->render('test/house_index.html.twig', [
+            'houses' => $houses
+        ]);
+    }
+
+    /**
+     * @Route({
      * "fr": "admin/house/creation",
      * "en": "admin/house/create"
      * }, name="house.create")
-     * @Route("/admin/house/{id}", name="house.edit", methods="GET|POST")
+     * @Route({
+     * "fr": "admin/house/{id}",
+     * "en": "admin/house/{id}"
+     * }, name="house.edit", methods="GET|POST")
      * @param Request $request
      * @param HouseRepository $repository
      * @param EntityManagerInterface $manager
@@ -52,5 +76,25 @@ class AdminHouseController extends AbstractController
         return $this->render('test/gites.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route({
+     * "fr": "admin/house/{id}",
+     * "en": "admin/house/{id}"
+     * }, name="house.delete", methods="DELETE")
+     * @param House $gites
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     *
+     * @return RedirectResponse
+     */
+    public function delete (House $gites, Request $request, EntityManagerInterface $manager) {
+        $manager->remove($gites);
+        $manager->flush();
+
+        //TODO: redirect to house index quand elle sera faite
+        //return $this->redirectToRoute('house.index');
+        return $this->redirectToRoute('home.index');
     }
 }
