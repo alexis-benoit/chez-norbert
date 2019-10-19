ee53216994c6551c9d291bc94e59e1d8a4c5131c
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-19 21:14:19 +0200

    :star2: feat: add user administration in admin index page

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -4,13 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Repository\HouseRepository;
 use App\Repository\MediaRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_USER")
  */
 class AdminController extends AbstractController
 {
@@ -18,15 +19,18 @@ class AdminController extends AbstractController
      * @Route("/admin", name="admin.index")
      * @param HouseRepository $houseRep
      * @param MediaRepository $mediaRep
+     * @param UserRepository $userRep
      * @return Response
      */
-    public function index (HouseRepository $houseRep, MediaRepository $mediaRep) {
+    public function index (HouseRepository $houseRep, MediaRepository $mediaRep, UserRepository $userRep) {
         $houses = $houseRep->getCount();
         $medias = $mediaRep->getCount();
+        $users = $userRep->getCount();
 
         return $this->render('admin/index.html.twig', [
             'house_count' => $houses,
-            'media_count' => $medias
+            'media_count' => $medias,
+            'user_count' => $users
         ]);
     }
 
