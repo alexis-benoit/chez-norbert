0e71534253b77f682cc3cfd4912009a291e6f183
FrancisDu68 <alexis.benoit.68@gmail.com>
2019-10-19 21:13:46 +0200

    :star2: feat: add admin user controller

---
+++ src/Controller/Admin/AdminUserController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
+class AdminUserController extends AbstractController
+{
+    /**
+     * @Route("/admin/user", name="admin.user.index")
+     * @param UserRepository $repository
+     * @return Response
+     */
+    public function index (UserRepository $repository) {
+        $users = $repository->findAll();
+
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/create", name="admin.user.create")
+     * @Route("/admin/user/{id}", name="admin.user.update", methods="GET|POST")
+     *
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param UserRepository $repository
+     * @param EntityManagerInterface $manager
+     * @param User|null $user
+     * @return Response
+     */
+    public function create(Request $request, UserPasswordEncoderInterface $passwordEncoder, UserRepository $repository, EntityManagerInterface $manager, User $user = null)
+    {
+        if (!$user) {
+            $user = new User();
+        }
+
+        $form = $this->createForm(UserType::class, $user, [
+            'edit' => !!$user->getId(),
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('admin.user.index');
+        }
+
+        return $this->render('admin/user/form.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/{id}", name="admin.user.delete", methods="DELETE")
+     *
+     * @param User $user
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function delete (User $user, EntityManagerInterface $manager, Request $request) {
+        $isCsrfValid = $this->isCsrfTokenValid(
+            'delete' . $user->getId(),
+            $request->get('_token')
+        );
+
+        if (!$isCsrfValid) {
+            $this->addFlash('danger', "Le jeton CSRF n'est pas valide.");
+            return $this->redirectToRoute('admin.user.index');
+        }
+
+        $manager->remove($user);
+        $manager->flush();
+
+        $this->addFlash('success', 'Le ' . $user->getType() . ' a été supprimé.');
+        return $this->redirectToRoute('admin.user.index');
+    }
+}
