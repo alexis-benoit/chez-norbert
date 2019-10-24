e7ade30dc1c74dcfdfaddd1822cedb685e8b1967
botflux <victor.mendele68@gmail.com>
2019-10-24 21:52:56 +0200

    :hammer: fix: fix image renaming issues in windows

--- src/DataFixtures/MediaFixture.php
+++ src/DataFixtures/MediaFixture.php
@@ -3,41 +3,38 @@
 namespace App\DataFixtures;
 
 use App\Entity\Media;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 class MediaFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-
         $httpClient = HttpClient::create();
         $faker = Factory::create();
 
         for ($i = 0; $i < 20; $i ++) {
 
             $response = $httpClient->request('GET', $faker->imageUrl());
-            $path = __DIR__ . "/../../public/$i.jpg";
+            $path = __DIR__ . "/../../public/images/medias/$i.jpg";
+            $publicPath = "$i.jpg";
             file_put_contents($path, $response->getContent());
 
-            $file = new UploadedFile($path, 'image', 'image/jpeg', null, true);
-
             $media = new Media();
-            $media->setName('tintin')
-                ->setAlt('a mulhouse')
-                ->setImageFile($file);
+            $media->setName($faker->name())
+                ->setAlt($faker->sentence())
+                ->setFilename($publicPath)
+                ->setUpdatedAt(new DateTimeImmutable());
 
 
             $manager->persist($media);
         }
-
-        //file_put_contents(, $response->getContent());
-
-        // $product = new Product();
-        // $manager->persist($product);
 
         $manager->flush();
     }
