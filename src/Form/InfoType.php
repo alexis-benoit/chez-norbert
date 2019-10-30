d9acfc1f9ce5080c3f2f5d922d93aa1d18ff017d
alexis-benoit <alexis.benoit.68@gmail.com>
2019-10-30 22:02:14 +0100

    :star2: feat: create form for web site information

---
+++ src/Form/InfoType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\WebSiteInformation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InfoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('siteName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('firstName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('phoneNumber', TelType::class)
+            ->add('address', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => WebSiteInformation::class,
+            'translation_domain' => 'forms',
+            'edit' => false,
+        ]);
+    }
+}
