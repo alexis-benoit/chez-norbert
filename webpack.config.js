88d6b252af26ab670a082dcfd48b923c775a0042
botflux <victor.mendele68@gmail.com>
2019-11-03 19:38:00 +0100

    :star2: feat: add boostrap using webpack encore

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,14 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/site/app.js')
+    .addEntry('app-admin', './assets/admin/app.js')
     .addStyleEntry('app-style', './assets/site/app.scss')
     .addStyleEntry('sm', './assets/site/sm.scss')
     .addStyleEntry('md', './assets/site/md.scss')
     .addStyleEntry('lg', './assets/site/lg.scss')
     .addStyleEntry('xl', './assets/site/xl.scss')
     .addStyleEntry('hover', './assets/site/hover.scss')
+    .addStyleEntry('app-admin-style', './assets/admin/app.scss')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
