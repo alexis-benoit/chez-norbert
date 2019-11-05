c5f3b4352b53c199c914f2b4e8c32750296ce687
Victor Mendele <victor.mendele68@gmail.com>
2019-11-05 20:51:32 +0100

    :herb: refactor: change booking form handling

--- src/Controller/HousesController.php
+++ src/Controller/HousesController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
 use App\Entity\House;
+use App\Form\BookingType;
 use App\Repository\HouseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,21 +18,32 @@ class HousesController extends AbstractController
      *
      * @param House $house
      * @param HouseRepository $repository
+     * @param Request $request
      * @return Response
      */
-    public function index(House $house, HouseRepository $repository)
+    public function index(House $house, HouseRepository $repository, Request $request)
     {
         //$anotherHouse = $repository->findAnotherHouse($house);
         $anotherHouses = $repository->findAnotherHouses($house);
         //$oppositeTypeHouses = $repository->findOppositeTypeHouses ($house);
         $oppositeTypeHouse = $repository->findOneOppositeTypeHouses ($house);
 
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Votre demande a bien été prise en compte.');
+        }
+
         return $this->render('houses/index.html.twig', [
             'house' => $house,
             //'anotherHouse' => $anotherHouse,
             'anotherHouses' => $anotherHouses,
             //'oppositeTypeHouses' => $oppositeTypeHouses
-            'oppositeTypeHouse' => $oppositeTypeHouse
+            'oppositeTypeHouse' => $oppositeTypeHouse,
+            'form' => $form->createView(),
         ]);
     }
 }
