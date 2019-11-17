6a80ceedca2ba9b9572dabbf459b48873a3df6da
botflux <victor.mendele68@gmail.com>
2019-11-17 14:26:00 +0100

    :hammer: fix: accept phone number starting by zero

--- src/Helper/PhoneNumberNormalizer.php
+++ src/Helper/PhoneNumberNormalizer.php
@@ -8,6 +8,11 @@ class PhoneNumberNormalizer
 {
     public static function normalize (string $phoneNumber) : string {
         $normalizedPhoneNumber = preg_replace('/\\s/', '', $phoneNumber);
+
+        if (substr($normalizedPhoneNumber, 0, 1) === '0') {
+            $normalizedPhoneNumber = sprintf('+33%s', substr($normalizedPhoneNumber, 1));
+        }
+
         return $normalizedPhoneNumber;
     }
 }
