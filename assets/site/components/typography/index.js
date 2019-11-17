ec881ab2a6f6c2b52119ea0543f92c36a915b31f
botflux <victor.mendele68@gmail.com>
2019-11-17 15:27:06 +0100

    :herb: refactor: use font loader to load material icons

--- assets/site/components/typography/index.js
+++ assets/site/components/typography/index.js
@@ -6,7 +6,7 @@ import WebFontLoader from 'webfontloader'
 const initializeWebFontLoader = ({}) => {
     WebFontLoader.load ({
         google: {
-            families: ['Montserrat:400,500,600', 'Playfair Display:400,700', 'Charmonman:400']
+            families: ['Montserrat:400,500,600', 'Playfair Display:400,700', 'Charmonman:400', 'Material Icons']
         }
     })
 }
