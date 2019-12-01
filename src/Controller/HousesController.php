26ee457a7a96394fa32391d0a1d5ab91d6b21729
botflux <victor.mendele68@gmail.com>
2019-12-01 15:47:31 +0100

    :star2: feat: add captcha in house page

--- src/Controller/HousesController.php
+++ src/Controller/HousesController.php
@@ -6,6 +6,11 @@ use App\Entity\Booking;
 use App\Entity\House;
 use App\Form\BookingType;
 use App\Repository\HouseRepository;
+use App\Repository\WebSiteInformationRepository;
+use App\Services\CaptchaVerifier;
+use App\Services\CaptchaVerifierInterface;
+use Swift_Mailer;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,10 +23,13 @@ class HousesController extends AbstractController
      *
      * @param House $house
      * @param HouseRepository $repository
+     * @param Swift_Mailer $mailer
      * @param Request $request
+     * @param CaptchaVerifierInterface $verifier
+     * @param WebSiteInformationRepository $informationRepository
      * @return Response
      */
-    public function index(House $house, HouseRepository $repository, Request $request)
+    public function index(House $house, HouseRepository $repository, Swift_Mailer $mailer,Request $request, CaptchaVerifierInterface $verifier, WebSiteInformationRepository $informationRepository)
     {
         //$anotherHouse = $repository->findAnotherHouse($house);
         $anotherHouses = $repository->findAnotherHouses($house);
@@ -33,8 +41,27 @@ class HousesController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid() && $verifier->verify($request->get('g-recaptcha-response'))) {
+            $message = (new Swift_Message())
+                ->setFrom($booking->getEmail())
+                ->setTo($informationRepository->findOne()->getEmail())
+                ->setBody(
+                    $this->renderView('mails/booking.html.twig', [
+                        'booking' => $booking,
+                        'house' => $house
+                    ]),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+
             $this->addFlash('success', 'Votre demande a bien été prise en compte.');
+            return $this->redirectToRoute('houses.get', [ 'slug' => $house->getSlug() ]);
+        }
+
+        if ($form->isSubmitted() &&  $form->isValid() && !$verifier->verify($request->get('g-recaptcha-response'))) {
+            $this->addFlash('danger', 'Captcha obligatoire');
         }
 
         return $this->render('houses/index.html.twig', [
